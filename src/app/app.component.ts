bf5e8bb60fe41cd2ba7001441e3ba25fd568c04a
M Gosschalk <monica.gosschalk@btinternet.com>
2018-03-18 17:02:04 +0000

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
 import { Component } from '@angular/core';
 
+interface Passenger {
+  id: number,
+  fullname: string,
+  checkedIn: boolean,
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  //title: string;
+  //groceries: string[];
+  //numberOne: number = 1;
+  //numberTwo: number = 2;
+  isHappy: boolean = false;
+  //logo: string = 'assets/img/logo.svg';
+  name: string = '';
+
+  passengers: Passenger[] = [
+    {
+      id: 1,
+      fullname: 'Stephen',
+      checkedIn: true
+    },
+    {
+      id: 2,
+      fullname: 'Rose',
+      checkedIn: false
+    },
+    {
+      id: 3,
+      fullname: 'James',
+      checkedIn: true
+    },
+    {
+      id: 4,
+      fullname: 'Louise',
+      checkedIn: true
+    },
+    {
+      id: 5,
+      fullname: 'Tina',
+      checkedIn: false
+    },
+  ];
+
+  constructor() {
+    //this.title = 'Ultimate Angular';
+  }
+
+  // handleBlur(event: any) {
+  //   this.name = event.target.value;
+  //   console.log(event);
+  // }
+
+  handleChange(value: string) {
+    this.name = value;
+  }
+
+  handleClick(value: string) {
+    //this.name = "Motto";
+    console.log(value);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
